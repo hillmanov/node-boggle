5bc475a0e83ad8501db9bd6848b6fd89487f3953
user <user@chromebook-hillman.(none)>
2012-06-26 18:14:34 -0600

    Modified test case

--- test/index.js
+++ test/index.js
@@ -2,7 +2,6 @@ var nb = require('../');
 
 describe("Test the boggle solver", function() { 
   it("Should solve the board and print all the word on the screen", function(done) {
-    
     var boggleSolver = new nb.BoggleSolver({
       letters: ['b', 'e', 'r', 't', 'g', 'h', 'n', 'm', 'a', 'e', 'w', 'p', 'l', 'e', 't', 'f'],
       minWordLength: 3
